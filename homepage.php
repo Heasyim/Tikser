698b118caa0f20456bfad90055d7e809fa4303d8
zankr <fauzankurniarahman@gmail.com>
2023-06-06 18:12:25 +0700

    update data

--- homepage.php
+++ homepage.php
@@ -7,16 +7,24 @@
     <link rel="shortcut icon" href="Assets/logo-tikser.svg" type="image/svg+xml">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">  
+    <link rel="stylesheet" href="style.css">
+    <script defer src="script.js"></script>
   </head>
-  <body class="bg-dark" id="page-top">
+  <body id="page-top">
+    <!-- Load Screen -->
+    <div class="spinner-wrapper">
+      <img src="Assets/logo-tikser.svg" alt="logo">
+      <div class="spinner-border text-warning" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-black bg-gradient fixed-top" id="mainNav">
-        <div class="container">
-            <a class="navbar-brand" href="#page-top">
-              <img src="Assets/logo-tikser.svg" class="img-fluid" width="114px" height="55.15px" alt="logo" >
-            </a>
+      <div class="container">
+        <a class="navbar-brand" href="#page-top">
+          <img src="Assets/logo-tikser.svg" class="img-fluid" width="114px" height="55.15px" alt="logo">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -41,13 +49,13 @@
               <a class="nav-link" href="#">CONTACT</a>
             </li>
           </ul>
-          <ul class="navbar-nav me-4" >
-           <li class="nav-item dropdown">
-              <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" >
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-menu-button-wide" viewBox="0 0 16 16">
-                <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v2A1.5 1.5 0 0 1 14.5 5h-13A1.5 1.5 0 0 1 0 3.5v-2zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-13z"/>
-                <path d="M2 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm10.823.323-.396-.396A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2H1zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2h14zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>
-              </svg>
+          <ul class="navbar-nav me-4">
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-menu-button-wide" viewBox="0 0 16 16">
+                  <path d="M0 1.5A1.5 1.5 0 0 1 1.5 0h13A1.5 1.5 0 0 1 16 1.5v2A1.5 1.5 0 0 1 14.5 5h-13A1.5 1.5 0 0 1 0 3.5v-2zM1.5 1a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-13z" />
+                  <path d="M2 2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm10.823.323-.396-.396A.25.25 0 0 1 12.604 2h.792a.25.25 0 0 1 .177.427l-.396.396a.25.25 0 0 1-.354 0zM0 8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8zm1 3v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2H1zm14-1V8a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2h14zM2 8.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+                </svg>
               </a>
               <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="#">LOGIN</a></li>
@@ -67,14 +75,42 @@
     </nav>
 
     <!-- Home -->
-    <div class="container mt-5 pt-2">
-        <div class="mt-4 p-5 bg-primary text-white rounded">
-        <h1>Popular Event</h1>
-        <hr class="my-4">
-        <a class="btn btn-warning btn-lg" href="#" role="button">BUY TICKET</a>
-
-      </div>
+      <!-- Carousel -->
+      <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="Assets/coldplay.png" class="d-block w-100" alt="coldplay">
+            <div class="carousel-caption d-none d-md-block ">
+            <button type="button" class="btn btn-warning btn-outline-dark">BUY TICKET</button>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="Assets/radwimps.png" class="d-block w-100" alt="radwimps">
+            <div class="carousel-caption d-none d-md-block">
+            <button type="button" class="btn btn-warning btn-outline-dark">BUY TICKET</button>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="Assets/the-weekend.png" class="d-block w-100" alt="weekend">
+            <div class="carousel-caption d-none d-md-block">
+            <button type="button" class="btn btn-warning btn-outline-dark">BUY TICKET</button>
+            </div>
+          </div>
+        </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    
+  <!-- List Konser -->
   </body>
 </html>
---
+++ script.js
@@ -0,0 +1,6 @@
+const spinnerWrapperE1 = document.querySelector('.spinner-wrapper');
+
+setTimeout(() => {
+    spinnerWrapperE1.style.opacity ='0';
+}, 1500)
+
