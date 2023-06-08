910ae7804ff4eeda525e1af637cfdbc184560e8a
zankr <fauzankurniarahman@gmail.com>
2023-06-08 14:19:30 +0700

    login sainap

---
+++ footer.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TIKSER</title>
+    <link rel="shortcut icon" href="Assets/logo-tikser.svg" type="image/svg+xml">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/footer.css">
+    <script defer src="script.js"></script>
+  </head>
+<body>
+    <!-- Footer -->
+    <section class="SecFooter">
+      <footer class="Footer">
+        <div class="Container1">
+          <div class="Satu">
+            <ul>
+              <li>
+                <img src="Assets/logo-tikser.svg" class="img-fluid" width="150px" height="60.15px" alt="logo">
+              </li>
+              <li class="WA">
+                <p>WhatsApp</p>
+                <p>0821 1234 5678<p>
+              </li>
+              <li class="Email">
+                <p>Email<p>
+                <p>tikser@gmail.com<p>
+              </li>
+            </ul>
+          </div>
+          <div class="Dua">
+            <ul>
+             <li>
+              <h3>Company</h3>
+             </li>
+             <li>
+              <a href="#">Partner</a> 
+             </li> 
+            </ul>
+          </div>
+          <div class="Tiga">
+            <ul>
+             <li>
+              <h3>Support</h3>
+             </li>
+             <li>
+              <a href="#">Help Center</a>
+             </li>
+             <li>
+              <a href="#">About Us</a>
+             </li> 
+             <li>
+              <a href="#">Terms & Condition</a> 
+             </li>
+             <li>
+              <a href="#">Privacy Policy</a>
+             </li>
+            </ul>
+          </div>
+          <div class="Empat">
+            <ul>
+             <li>
+             <h3>Get TIKSER app</h3>  
+             </li>
+             <li>
+             <img src="Assets/GooglePlay.svg" class="img-fluid" width="150px" height="60.15px" alt="logo">
+             </li>
+             <li>
+             <img src="Assets/AppStore.svg" class="img-fluid" width="150px" height="60.15px" alt="logo">
+             </li> 
+            </ul>
+          </div>
+        </div>
+        <div class="Container2">
+          <div class="Partner">
+            
+            <h3>Partner</h3>
+            <div class="sponsor">
+              <img src="Assets/BCA-TESLA.svg" width="350px" height="60.15px" alt="">
+              <img src="Assets/Wekidi-Modify.svg" width="150px" height="60.15px" alt="">
+            </div>
+            
+          </div>
+          <div class="FollowUS">
+            <h3>Follow US</h3>
+            <div class="sosmed">
+              <a href="#"><img src="Assets/facebook.png"  alt=""></a>
+              <a href="#"><img src="Assets/twitter.png"  alt=""></a>
+              <a href="#"><img src="Assets/instagram.png"  alt=""></a>
+              <a href="#"><img src="Assets/youtube.png"  alt=""></a>
+            </div>
+          </div>
+        </div>
+        <div class="copyright">
+          <img src="Assets/logo-tikser.svg" width="150px" height="60.15px" alt="">
+          <h3>© 2011-2023 All Rights Reserved.</h3>
+        </div>
+      </footer>
+    </section>
+
+    
+</body>
+</html>
